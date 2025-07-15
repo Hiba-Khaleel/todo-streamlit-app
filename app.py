6f825a02fb4852ed7b442feac8896760bfbd8b41
Hiba Khaleel <hb.khaleel@gmail.com>
2025-07-15 13:39:34 +0200

    initial code

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos(filepath="todos.txt")
+st.title("my todo app")
+st.subheader('this app is to manage the daily tasks')
+
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="add new todo", placeholder="add todo")
