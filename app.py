3eb02beeb0e4da3211e44b15e84ac49913c097db
Hiba Khaleel <hb.khaleel@gmail.com>
2025-07-17 10:10:30 +0200

    add complate function

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 import streamlit as st
 import functions
 
-todos = functions.get_todos(filepath="todos.txt")
+
+def addtodo():
+    todo = st.session_state["new_todo"]+'\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+    print(todo)
+
+
+todos = functions.get_todos()
 st.title("my todo app")
 st.subheader('this app is to manage the daily tasks')
 st.subheader('this app is to manage the daily tasks')
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
-st.text_input(label="add new todo", placeholder="add todo")
+
+st.text_input(label="add new todo", placeholder="add todo",
+              on_change=addtodo, key="new_todo")
+
+st.session_state
